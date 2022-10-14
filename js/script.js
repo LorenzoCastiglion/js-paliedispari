6a9d47bc5b7f0b2058a3695e4e8eb90183bc248a
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-10-14 15:19:26 +0200

    impostazione esercizio

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+  const container = document.querySelector('.container');
+ 
+const bottone = document.querySelector('button');
+const inputTag = document.getElementById('numero');
+const testo = document.querySelector('h1');
+
+bottone.addEventListener('click', function(){
+    const num = parseInt(inputTag.value);
+    
+   if(isNaN(num)){
+    testo.innerHTML = '';
+    const divAlert = notificationError('Devi inserire un  umero !');
+    container.prepend(divAlert);
+    return;
+   }
+   removeFirstNotification();
+   if(isEven(num)){
+       testo.innerHTML = 'Il numero è pari !';
+   } else {
+        testo.innerHTML = 'Il numero è dispari !';
+   }
+})
